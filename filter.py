14c4582484db606967cec6a4af7b736cb76e39d0
Дарья <alis131140@gmail.com>
2022-09-17 00:52:06 +0300

    1

---
+++ filter.py
@@ -0,0 +1,31 @@
+from out import ConvertFromDictToCLass
+import json
+
+with open('bd.json', 'r') as f:
+    res = json.load(f)
+
+result = ConvertFromDictToCLass(res)['CHILD']
+
+criteria = input('Enter criteria separated by a space (For example: name-Daria age-19): ')
+criteria = criteria.strip().split(' ')
+
+for i in result:
+    a = 0
+    for j in criteria:
+        criter = j.split('-')
+        if criter[0] == 'name':
+            if i.name == criter[1]:
+                a += 1
+        if criter[0] == 'age':
+            if i.age == int(criter[1]):
+                a += 1
+        if criter[0] == 'squad':
+            if i.name == criter[1]:
+                a += 1
+        if criter[0] == 'phone':
+            if i.name == criter[1]:
+                a += 1
+        if a == 0:
+            break
+    if a == 2:
+        print(i)
